bd359dfe0f66be1520c5b4fe0b9a931b85f6fe59
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-23 15:59:50 -0300

    Service layer, component registration

---
+++ src/main/java/com/projectspring/couse/services/UserService.java
@@ -0,0 +1,26 @@
+package com.projectspring.couse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projectspring.couse.entities.User;
+import com.projectspring.couse.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
