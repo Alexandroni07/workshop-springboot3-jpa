738e22010a9538a1c96f55d3af1e1b005f4e31c4
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-23 14:31:26 -0300

    Project Created

---
+++ src/main/java/com/projectspring/couse/CouseApplication.java
@@ -0,0 +1,13 @@
+package com.projectspring.couse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectspring/couse/CouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectspring.couse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
