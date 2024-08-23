5d945f825198234539f85de70eb6213323279c86
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-23 17:30:12 -0300

    Product

--- src/main/java/com/projectspring/couse/config/TestConfig.java
+++ src/main/java/com/projectspring/couse/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.projectspring.couse.entities.Category;
 import com.projectspring.couse.entities.Order;
+import com.projectspring.couse.entities.Product;
 import com.projectspring.couse.entities.User;
 import com.projectspring.couse.entities.enums.OrderStatus;
 import com.projectspring.couse.repositories.CategoryRepository;
 import com.projectspring.couse.repositories.OrderRepository;
+import com.projectspring.couse.repositories.ProductRepository;
 import com.projectspring.couse.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -42,6 +47,15 @@ public class TestConfig implements CommandLineRunner {
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/projectspring/couse/entities/Product.java
+++ src/main/java/com/projectspring/couse/entities/Product.java
@@ -2,7 +2,6 @@ package com.projectspring.couse.entities;
 
 import java.io.Serializable;
 import java.util.HashSet;
-
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -13,26 +12,31 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category  implements Serializable  {
-	
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+	private String description;
+	private Double price;
+	private String imgUrl;
+
 	@Transient
-	private Set<Product> products = new HashSet<>();
-	
-	public Category() {
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -50,9 +54,33 @@ public class Category  implements Serializable  {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public Set<Product> getProducts() {
-		return products;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -71,7 +99,7 @@ public class Category  implements Serializable  {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -79,7 +107,4 @@ public class Category  implements Serializable  {
 			return false;
 		return true;
 	}
-	
-	
-
 }
