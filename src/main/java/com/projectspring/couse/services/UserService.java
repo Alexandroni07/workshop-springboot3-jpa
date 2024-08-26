82658481f1784352219cd83b0cf534e9ba8e3b2e
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-26 12:45:00 -0300

    Exception handling - update

--- src/main/java/com/projectspring/couse/services/UserService.java
+++ src/main/java/com/projectspring/couse/services/UserService.java
@@ -13,6 +13,8 @@ import com.projectspring.couse.repositories.UserRepository;
 import com.projectspring.couse.services.exceptions.DatabaseException;
 import com.projectspring.couse.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
