e98b95ef07efc002b5c670035b9830d2f106fcc0
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-26 10:03:44 -0300

    Payment, one-to-one

--- src/main/java/com/projectspring/couse/config/TestConfig.java
+++ src/main/java/com/projectspring/couse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projectspring.couse.entities.Category;
 import com.projectspring.couse.entities.Order;
 import com.projectspring.couse.entities.OrderItem;
+import com.projectspring.couse.entities.Payment;
 import com.projectspring.couse.entities.Product;
 import com.projectspring.couse.entities.User;
 import com.projectspring.couse.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
