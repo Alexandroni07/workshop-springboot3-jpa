1a3c1c9e0e4e512a70f9f5f27591c114002db25f
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-26 11:50:09 -0300

    User insert

--- src/main/java/com/projectspring/couse/services/UserService.java
+++ src/main/java/com/projectspring/couse/services/UserService.java
@@ -23,4 +23,10 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	
 }
