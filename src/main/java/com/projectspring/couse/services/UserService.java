c4f0c5c910ca4b6c90432f9488e07f7420474b3f
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-26 12:34:14 -0300

    Exception handling - findById

--- src/main/java/com/projectspring/couse/services/UserService.java
+++ src/main/java/com/projectspring/couse/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.projectspring.couse.entities.User;
 import com.projectspring.couse.repositories.UserRepository;
+import com.projectspring.couse.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
